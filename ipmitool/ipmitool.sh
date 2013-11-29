c630f4cb6a72a5798120a208cf53e3ea57c2c3b3
Matthias Breddin <mb@lunetics.com>
2013-11-29 12:11:34 +0100

    Missing space

--- ipmitool/ipmitool.sh
+++ ipmitool/ipmitool.sh
@@ -39,7 +39,7 @@ do
         	if (tolower($3) ~ /watt/) type="power";
         	if (tolower($3) ~ /degree/) type="temperature";
         	gsub(/[ \t]*$/,"",$1) gsub(/[ \t-]+/,"_",$1);
-        	print "PUTVAL "host"/ipmitool/" type "/" $1 " interval=" interval  "N:" sprintf("%.4f",$2) ":U"  
+        	print "PUTVAL "host"/ipmitool/" type "/" $1 " interval=" interval  " N:" sprintf("%.4f",$2) ":U"
         }'
 
         sleep ${COLLECTD_INTERVAL:-10} || true;
